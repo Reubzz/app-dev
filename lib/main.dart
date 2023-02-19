f102ff1a4dc0348c733254e1238964b144a06dfa
Reubz <reugeorac@gmail.com>
2023-02-19 18:16:53 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+
+
+// Copyright (c) 2019, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import '/movies/ui/pages/MovieListPage.dart';
+import 'movies/data/datasource/fake_data.dart';
+import 'movies/data/models/movie_model.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.orange,
+        ),
+        home: MovieListPage(movies));
+  }
+}
